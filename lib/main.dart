ed447670c4baf43f22695270750eeb729dc55b5c
Victor Henzzo <victorhenzzowp@gmail.com>
2023-12-04 11:28:18 -0300

    Removing debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,11 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(final BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Challenge Mobile Developer',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
