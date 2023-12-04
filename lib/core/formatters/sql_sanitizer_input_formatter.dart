c882452d557b01d4789ba660e3597941f0b21ded
Victor Henzzo <victorhenzzowp@gmail.com>
2023-12-04 16:31:39 -0300

    creating SqlSanitizerInputFormatter

---
+++ lib/core/formatters/sql_sanitizer_input_formatter.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/services.dart';
+
+class SqlSanitizerInputFormatter extends TextInputFormatter {
+  @override
+  TextEditingValue formatEditUpdate(
+    final TextEditingValue oldValue,
+    final TextEditingValue newValue,
+  ) {
+    final sanitizedText = sanitizeInput(newValue.text);
+
+    return TextEditingValue(
+      text: sanitizedText,
+      selection: TextSelection.collapsed(offset: sanitizedText.length),
+    );
+  }
+
+  String sanitizeInput(final String input) {
+    return input.replaceAll(RegExp("[';-]"), '');
+  }
+}
