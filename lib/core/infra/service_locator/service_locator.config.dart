6c5d059b1ae4f1904d36ca25eac2e5c47a8ec7e9
Victor Henzzo <victorhenzzowp@gmail.com>
2023-12-05 13:15:51 -0300

    Adding edit student bloc into service locator

--- lib/core/infra/service_locator/service_locator.config.dart
+++ lib/core/infra/service_locator/service_locator.config.dart
@@ -13,53 +13,59 @@ import 'package:challenge_mobile_developer/core/data/datasources/http_data_sourc
 import 'package:challenge_mobile_developer/core/data/datasources/http_data_source/http_data_source_adapter.dart'
     as _i6;
 import 'package:challenge_mobile_developer/core/data/datasources/local_data_source/local_data_source.dart'
-    as _i11;
+    as _i12;
 import 'package:challenge_mobile_developer/core/data/datasources/local_data_source/shared_preferences_local_data_source.dart'
     as _i9;
 import 'package:challenge_mobile_developer/core/data/repositories/students_repository_impl.dart'
-    as _i10;
+    as _i11;
 import 'package:challenge_mobile_developer/core/data/repositories/user_repository_impl.dart'
-    as _i13;
-import 'package:challenge_mobile_developer/core/domain/repositories/students_repository.dart'
-    as _i12;
-import 'package:challenge_mobile_developer/core/domain/repositories/user_repository.dart'
-    as _i18;
-import 'package:challenge_mobile_developer/core/domain/use_cases/check_auth_state_use_case.dart'
-    as _i19;
-import 'package:challenge_mobile_developer/core/domain/use_cases/create_student_use_case.dart'
     as _i14;
-import 'package:challenge_mobile_developer/core/domain/use_cases/delete_student_use_case.dart'
+import 'package:challenge_mobile_developer/core/domain/repositories/students_repository.dart'
+    as _i13;
+import 'package:challenge_mobile_developer/core/domain/repositories/user_repository.dart'
+    as _i19;
+import 'package:challenge_mobile_developer/core/domain/use_cases/check_auth_state_use_case.dart'
+    as _i20;
+import 'package:challenge_mobile_developer/core/domain/use_cases/create_student_use_case.dart'
     as _i15;
-import 'package:challenge_mobile_developer/core/domain/use_cases/edit_student_use_case.dart'
+import 'package:challenge_mobile_developer/core/domain/use_cases/delete_student_use_case.dart'
     as _i16;
-import 'package:challenge_mobile_developer/core/domain/use_cases/fetch_students_use_case.dart'
+import 'package:challenge_mobile_developer/core/domain/use_cases/edit_student_use_case.dart'
     as _i17;
+import 'package:challenge_mobile_developer/core/domain/use_cases/fetch_students_use_case.dart'
+    as _i18;
 import 'package:challenge_mobile_developer/core/domain/use_cases/sign_in_use_case.dart'
-    as _i22;
+    as _i23;
 import 'package:challenge_mobile_developer/core/infra/bindings/http_data_source_module.dart'
-    as _i27;
-import 'package:challenge_mobile_developer/core/infra/bindings/local_data_source_module.dart'
-    as _i28;
-import 'package:challenge_mobile_developer/core/infra/bindings/students_module.dart'
-    as _i29;
-import 'package:challenge_mobile_developer/core/infra/bindings/user_module.dart'
     as _i30;
+import 'package:challenge_mobile_developer/core/infra/bindings/local_data_source_module.dart'
+    as _i31;
+import 'package:challenge_mobile_developer/core/infra/bindings/students_module.dart'
+    as _i32;
+import 'package:challenge_mobile_developer/core/infra/bindings/user_module.dart'
+    as _i33;
 import 'package:challenge_mobile_developer/modules/home/navigation/home_screen_directions.dart'
     as _i4;
 import 'package:challenge_mobile_developer/modules/home/presentation/home_dependencies_bloc/home_dependencies_bloc.dart'
-    as _i21;
+    as _i22;
 import 'package:challenge_mobile_developer/modules/home/presentation/home_dependencies_presenter.dart'
-    as _i20;
+    as _i21;
 import 'package:challenge_mobile_developer/modules/home/presentation/students_manager_bloc/students_manager_bloc.dart'
-    as _i24;
+    as _i25;
 import 'package:challenge_mobile_developer/modules/home/presentation/students_manager_presenter.dart'
-    as _i23;
+    as _i24;
 import 'package:challenge_mobile_developer/modules/login/navigation/login_screen_directions.dart'
     as _i7;
 import 'package:challenge_mobile_developer/modules/login/presentation/bloc/login_bloc.dart'
-    as _i26;
+    as _i29;
 import 'package:challenge_mobile_developer/modules/login/presentation/login_presenter.dart'
-    as _i25;
+    as _i28;
+import 'package:challenge_mobile_developer/modules/student_edit/edit_student_presenter.dart'
+    as _i26;
+import 'package:challenge_mobile_developer/modules/student_edit/navigation/student_edit_directions.dart'
+    as _i10;
+import 'package:challenge_mobile_developer/modules/student_edit/presentation/bloc/edit_student_bloc.dart'
+    as _i27;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:http/http.dart' as _i3;
 import 'package:injectable/injectable.dart' as _i2;
@@ -93,61 +99,67 @@ extension GetItInjectableX on _i1.GetIt {
     gh.factory<_i9.SharedPreferencesLocalDataSource>(() =>
         _i9.SharedPreferencesLocalDataSource(
             sharedPreferences: gh<_i8.SharedPreferences>()));
-    gh.factory<_i10.StudentsRepositoryImpl>(
-        () => _i10.StudentsRepositoryImpl(gh<_i5.HttpDataSource>()));
-    gh.factory<_i11.LocalDataSource>(() => localDataSourceModule
+    gh.factory<_i10.StudentEditDirections>(() => _i10.StudentEditDirections());
+    gh.factory<_i11.StudentsRepositoryImpl>(
+        () => _i11.StudentsRepositoryImpl(gh<_i5.HttpDataSource>()));
+    gh.factory<_i12.LocalDataSource>(() => localDataSourceModule
         .localDataSource(gh<_i9.SharedPreferencesLocalDataSource>()));
-    gh.factory<_i12.StudentsRepository>(() =>
-        studentsModule.studentsRepository(gh<_i10.StudentsRepositoryImpl>()));
-    gh.factory<_i13.UserRepositoryImpl>(() => _i13.UserRepositoryImpl(
+    gh.factory<_i13.StudentsRepository>(() =>
+        studentsModule.studentsRepository(gh<_i11.StudentsRepositoryImpl>()));
+    gh.factory<_i14.UserRepositoryImpl>(() => _i14.UserRepositoryImpl(
           gh<_i5.HttpDataSource>(),
-          gh<_i11.LocalDataSource>(),
+          gh<_i12.LocalDataSource>(),
         ));
-    gh.factory<_i14.CreateStudentUseCaseImpl>(
-        () => _i14.CreateStudentUseCaseImpl(gh<_i12.StudentsRepository>()));
-    gh.factory<_i15.DeleteStudentUseCaseImpl>(
-        () => _i15.DeleteStudentUseCaseImpl(gh<_i12.StudentsRepository>()));
-    gh.factory<_i16.EditStudentUseCaseImpl>(
-        () => _i16.EditStudentUseCaseImpl(gh<_i12.StudentsRepository>()));
-    gh.factory<_i17.FetchStudentsUseCaseImpl>(
-        () => _i17.FetchStudentsUseCaseImpl(gh<_i12.StudentsRepository>()));
-    gh.factory<_i18.UserRepository>(
-        () => authModule.authRepository(gh<_i13.UserRepositoryImpl>()));
-    gh.factory<_i19.CheckAuthStateUseCaseImpl>(
-        () => _i19.CheckAuthStateUseCaseImpl(gh<_i18.UserRepository>()));
-    gh.factory<_i14.CreateStudentUseCase>(() => studentsModule
-        .createStudentUseCase(gh<_i14.CreateStudentUseCaseImpl>()));
-    gh.factory<_i15.DeleteStudentUseCase>(() => studentsModule
-        .deleteStudentUseCase(gh<_i15.DeleteStudentUseCaseImpl>()));
-    gh.factory<_i16.EditStudentUseCase>(() =>
-        studentsModule.editStudentUseCase(gh<_i16.EditStudentUseCaseImpl>()));
-    gh.factory<_i17.FetchStudentsUseCase>(() => studentsModule
-        .fetchStudentsUseCase(gh<_i17.FetchStudentsUseCaseImpl>()));
-    gh.factory<_i20.HomeDependenciesPresenter>(() => _i21.HomeDependenciesBloc(
-        fetchStudentsUseCase: gh<_i17.FetchStudentsUseCase>()));
-    gh.factory<_i22.SignInUseCaseImpl>(
-        () => _i22.SignInUseCaseImpl(gh<_i18.UserRepository>()));
-    gh.factory<_i23.StudentsManagerPresenter>(() => _i24.StudentsManagerBloc(
-          deleteStudentUseCase: gh<_i15.DeleteStudentUseCase>(),
+    gh.factory<_i15.CreateStudentUseCaseImpl>(
+        () => _i15.CreateStudentUseCaseImpl(gh<_i13.StudentsRepository>()));
+    gh.factory<_i16.DeleteStudentUseCaseImpl>(
+        () => _i16.DeleteStudentUseCaseImpl(gh<_i13.StudentsRepository>()));
+    gh.factory<_i17.EditStudentUseCaseImpl>(
+        () => _i17.EditStudentUseCaseImpl(gh<_i13.StudentsRepository>()));
+    gh.factory<_i18.FetchStudentsUseCaseImpl>(
+        () => _i18.FetchStudentsUseCaseImpl(gh<_i13.StudentsRepository>()));
+    gh.factory<_i19.UserRepository>(
+        () => authModule.authRepository(gh<_i14.UserRepositoryImpl>()));
+    gh.factory<_i20.CheckAuthStateUseCaseImpl>(
+        () => _i20.CheckAuthStateUseCaseImpl(gh<_i19.UserRepository>()));
+    gh.factory<_i15.CreateStudentUseCase>(() => studentsModule
+        .createStudentUseCase(gh<_i15.CreateStudentUseCaseImpl>()));
+    gh.factory<_i16.DeleteStudentUseCase>(() => studentsModule
+        .deleteStudentUseCase(gh<_i16.DeleteStudentUseCaseImpl>()));
+    gh.factory<_i17.EditStudentUseCase>(() =>
+        studentsModule.editStudentUseCase(gh<_i17.EditStudentUseCaseImpl>()));
+    gh.factory<_i18.FetchStudentsUseCase>(() => studentsModule
+        .fetchStudentsUseCase(gh<_i18.FetchStudentsUseCaseImpl>()));
+    gh.factory<_i21.HomeDependenciesPresenter>(() => _i22.HomeDependenciesBloc(
+        fetchStudentsUseCase: gh<_i18.FetchStudentsUseCase>()));
+    gh.factory<_i23.SignInUseCaseImpl>(
+        () => _i23.SignInUseCaseImpl(gh<_i19.UserRepository>()));
+    gh.factory<_i24.StudentsManagerPresenter>(() => _i25.StudentsManagerBloc(
+          deleteStudentUseCase: gh<_i16.DeleteStudentUseCase>(),
           directions: gh<_i4.HomeScreenDirections>(),
         ));
-    gh.factory<_i19.CheckAuthStateUseCase>(() =>
-        authModule.checkAuthStateUseCase(gh<_i19.CheckAuthStateUseCaseImpl>()));
-    gh.factory<_i22.SignInUseCase>(
-        () => authModule.signInUseCase(gh<_i22.SignInUseCaseImpl>()));
-    gh.factory<_i25.LoginPresenter>(() => _i26.LoginBloc(
-          checkAuthStateUseCase: gh<_i19.CheckAuthStateUseCase>(),
-          signInUseCase: gh<_i22.SignInUseCase>(),
+    gh.factory<_i20.CheckAuthStateUseCase>(() =>
+        authModule.checkAuthStateUseCase(gh<_i20.CheckAuthStateUseCaseImpl>()));
+    gh.factory<_i26.EditStudentPresenter>(() => _i27.EditStudentBloc(
+          directions: gh<_i10.StudentEditDirections>(),
+          createStudentUseCase: gh<_i15.CreateStudentUseCase>(),
+          editStudentUseCase: gh<_i17.EditStudentUseCase>(),
+        ));
+    gh.factory<_i23.SignInUseCase>(
+        () => authModule.signInUseCase(gh<_i23.SignInUseCaseImpl>()));
+    gh.factory<_i28.LoginPresenter>(() => _i29.LoginBloc(
+          checkAuthStateUseCase: gh<_i20.CheckAuthStateUseCase>(),
+          signInUseCase: gh<_i23.SignInUseCase>(),
           directions: gh<_i7.LoginScreenDirections>(),
         ));
     return this;
   }
 }
 
-class _$HttpDataSourceModule extends _i27.HttpDataSourceModule {}
+class _$HttpDataSourceModule extends _i30.HttpDataSourceModule {}
 
-class _$LocalDataSourceModule extends _i28.LocalDataSourceModule {}
+class _$LocalDataSourceModule extends _i31.LocalDataSourceModule {}
 
-class _$StudentsModule extends _i29.StudentsModule {}
+class _$StudentsModule extends _i32.StudentsModule {}
 
-class _$AuthModule extends _i30.AuthModule {}
+class _$AuthModule extends _i33.AuthModule {}
--- lib/modules/student_edit/presentation/bloc/edit_student_bloc.dart
+++ lib/modules/student_edit/presentation/bloc/edit_student_bloc.dart
@@ -1,17 +1,33 @@
 import 'package:bloc/bloc.dart';
+import 'package:challenge_mobile_developer/core/domain/entities/student_entity.dart';
+import 'package:challenge_mobile_developer/core/domain/parameters/create_student_params.dart';
+import 'package:challenge_mobile_developer/core/domain/parameters/edit_student_params.dart';
+import 'package:challenge_mobile_developer/core/domain/use_cases/create_student_use_case.dart';
+import 'package:challenge_mobile_developer/core/domain/use_cases/edit_student_use_case.dart';
 import 'package:challenge_mobile_developer/modules/student_edit/edit_student_presenter.dart';
+import 'package:challenge_mobile_developer/modules/student_edit/navigation/student_edit_directions.dart';
 import 'package:equatable/equatable.dart';
+import 'package:injectable/injectable.dart';
 
 part 'edit_student_event.dart';
 part 'edit_student_state.dart';
 
+@Injectable(as: EditStudentPresenter)
 class EditStudentBloc extends Bloc<EditStudentEvent, EditStudentState>
     implements EditStudentPresenter {
-  EditStudentBloc() : super(EditStudentInitialState()) {
+  EditStudentBloc({
+    required this.directions,
+    required this.createStudentUseCase,
+    required this.editStudentUseCase,
+  }) : super(const EditStudentInitialState()) {
     on<EditStudentEvent>((event, emit) {
       // TODO: implement event handler
     });
   }
+
+  final StudentEditDirections directions;
+  final CreateStudentUseCase createStudentUseCase;
+  final EditStudentUseCase editStudentUseCase;
 
   @override
   void addEvent(final EditStudentEvent event) {
